f1289421cd240147059f4f1d4e6b5451d82b213b
IXLives <bryant.ian87@gmail.com>
2020-09-22 15:26:17 -0400

    paws

--- hello/app.py
+++ hello/app.py
@@ -13,5 +13,10 @@ def greetings(my_name):
     return 'Welcome, ' + my_name + '!'
 
 
+@app.route('/square/<int:number>')
+def show_square(number):
+    return "Square of " + str(number) + " is: " + str(number * number)
+
+
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=3000)
