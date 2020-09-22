d549f337e97faedc6450baafa0bb7cc3c0ef1d52
IXLives <bryant.ian87@gmail.com>
2020-09-22 17:21:10 -0400

    jinja

--- paws/app.py
+++ paws/app.py
@@ -2,11 +2,20 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+pets = [
+    {"id": 1, "name": "Stuie", "age": "5 weeks",
+        "bio": "I am a tiny kitten rescued by the good people at Kitten Bizness. I love squeaky toys and cuddles."},
+    {"id": 2, "name": "Karev", "age": "8 months",
+     "bio": "I am a handsome gentle-cat. I like to dress up in bow ties."},
+    {"id": 3, "name": "Bruce", "age": "1 year",
+     "bio": "I love barking. But, I love my friends more."},
+    {"id": 4, "name": "Wayne", "age": "5 years", "bio": "Probably napping."},
+]
+
 
 @app.route('/')
 def home():
-    return render_template('home.html')
-    # Kitten Bizness Trap-Neuter-Return🐾
+    return render_template('home.html', pets=pets)
 
 
 @app.route('/about')
